81f7640bf7b8e14f55a1c32844fe1e6884124ccf
getLion <1627184693@qq.com>
2019-04-25 14:28:18 +0800

    button任务完成

--- src/views/component/Button.vue
+++ src/views/component/Button.vue
@@ -2,6 +2,20 @@
   <div class="main">
     <el-row>
       <el-button type="success">成功按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+    </el-row>
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button size="medium">中等按钮</el-button>
+      <el-button size="small">小型按钮</el-button>
+    </el-row>
+    <el-row>
+      <el-button type="text">文字按钮</el-button>
+    </el-row>
+    <el-row>
+      <el-button disabled>禁用按钮</el-button>
     </el-row>
   </div>
 </template>
@@ -15,4 +29,5 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+</style>
