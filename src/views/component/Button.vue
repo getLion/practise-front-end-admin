c002cb8e6a54dcb3a11015b17d986e8ec09aa275
getLion <1627184693@qq.com>
2019-04-25 14:01:31 +0800

    增加成功按钮

--- src/views/component/Button.vue
+++ src/views/component/Button.vue
@@ -1,19 +1,18 @@
 <template>
   <div class="main">
+    <el-row>
+      <el-button type="success">成功按钮</el-button>
+    </el-row>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {
-
-    }  
+    return {};
   },
-  methods: {
-    
-  }
-}
+  methods: {}
+};
 </script>
 
 <style scoped></style>
