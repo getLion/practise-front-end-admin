1c1e7f58050faa58d6e06963577860655041abc3
getLion <1627184693@qq.com>
2019-04-25 15:06:23 +0800

    table任务完成

--- src/views/component/Table.vue
+++ src/views/component/Table.vue
@@ -1,26 +1,9 @@
 <template>
   <div class="main">
-    <el-table
-      :data="list"
-      border
-      align="center"
-      stripe>
-      <el-table-column
-        type="index"
-        label="序列"
-        align="center"
-        width="80">
-      </el-table-column>
-      <el-table-column
-        prop="name"
-        label="姓名"
-        >
-      </el-table-column>
-      <el-table-column
-        prop="gender"
-        label="性别"
-        >
-      </el-table-column>
+    <el-table :data="list" border align="center" stripe>
+      <el-table-column type="index" label="序列" align="center" width="80"></el-table-column>
+      <el-table-column prop="name" label="姓名"></el-table-column>
+      <el-table-column prop="gender" label="性别" :formatter="formatSex"></el-table-column>
     </el-table>
   </div>
 </template>
@@ -29,22 +12,34 @@
 export default {
   data() {
     return {
-      list: [{
-        name: 'Joel',
-        gender: 1
-      }, {
-        name: 'Mary',
-        gender: 2
-      }, {
-        name: 'Jimmy',
-        gender: 3
-      }, ]
-    }  
+      list: [
+        {
+          name: "Joel",
+          gender: 1
+        },
+        {
+          name: "Mary",
+          gender: 2
+        },
+        {
+          name: "Jimmy",
+          gender: 3
+        }
+      ]
+    };
   },
   methods: {
-    
+    formatSex: function(row, column) {
+      if (row.gender === 1) {
+        return "男";
+      } else if (row.gender === 2) {
+        return "女";
+      } else {
+        return "未知";
+      }
+    }
   }
-}
+};
 </script>
 
 <style scoped></style>
