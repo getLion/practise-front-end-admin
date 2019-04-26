26b86206ec828537eca4c35fe2565577b6c4c509
getLion <1627184693@qq.com>
2019-04-26 19:35:54 +0800

    input任务完成

--- src/views/component/Input.vue
+++ src/views/component/Input.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="main">
+    <el-input v-model="input" placeholder="请输入内容"></el-input>
+    <el-input type="textarea" :rows="2" placeholder="请输入内容" v-model="textarea"></el-input>
+    <el-input type="number" v-model="num" placeholder="请输入数字"></el-input>
   </div>
 </template>
 
@@ -7,13 +10,21 @@
 export default {
   data() {
     return {
-
-    }  
+      input: "",
+      textarea: "",
+      num: ""
+    };
   },
-  methods: {
-    
-  }
-}
+  methods: {}
+};
 </script>
 
-<style scoped></style>
+<style>
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+}
+input[type="number"] {
+  -moz-appearance: textfield;
+}
+</style>
