6a5ec691964d01203175d77d94af37547b9b86f4
getLion <1627184693@qq.com>
2019-04-26 19:45:26 +0800

    radio-checkbox任务完成

--- src/views/component/Radio-checkbox.vue
+++ src/views/component/Radio-checkbox.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="main">
+    <el-radio-group v-model="radio">
+      <el-radio :label="3">备选项</el-radio>
+      <el-radio :label="6">备选项</el-radio>
+      <el-radio :label="9">备选项</el-radio>
+    </el-radio-group>
+    <el-checkbox-group v-model="checkList">
+      <el-checkbox label="复选框 A"></el-checkbox>
+      <el-checkbox label="复选框 B"></el-checkbox>
+      <el-checkbox label="复选框 C"></el-checkbox>
+    </el-checkbox-group>
   </div>
 </template>
 
@@ -7,13 +17,12 @@
 export default {
   data() {
     return {
-
-    }  
+      radio: "",
+      checkList: []
+    };
   },
-  methods: {
-    
-  }
-}
+  methods: {}
+};
 </script>
 
 <style scoped></style>
