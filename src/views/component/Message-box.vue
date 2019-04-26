e76c5f9c8ca3d09947a0015e18acc7bbdb3ff96a
getLion <1627184693@qq.com>
2019-04-26 18:44:29 +0800

    box弹框任务完成

--- src/views/component/Message-box.vue
+++ src/views/component/Message-box.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="main">
-    <el-button type="danger">删除</el-button>
+    <el-button type="danger" @click="open">删除</el-button>
     <br>
     姓名: {{name}}
-    <el-button :plain="true">录入姓名</el-button>
-
+    <el-button :plain="true" @click="open2">录入姓名</el-button>
   </div>
 </template>
 
@@ -12,13 +11,48 @@
 export default {
   data() {
     return {
-      name: '未设置'
-    }  
+      name: "未设置"
+    };
   },
   methods: {
-    
+    open() {
+      this.$confirm("确定删除？", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          this.name = "未设置"
+          this.$message({
+            type: "success",
+            message: "删除成功!"
+          });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除"
+          });
+        });
+    },
+    open2() {
+        this.$prompt('请输入姓名', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消'
+        }).then(({ value }) => {
+          this.name= value
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '取消输入'
+          });       
+        });
+      }
+  },
+  mounted() {
+    this.$message("本网站基于 Vue 开发");
   }
-}
+};
 </script>
 
 <style scoped></style>
--- src/views/component/Message.vue
+++ src/views/component/Message.vue
@@ -10,33 +10,31 @@
 <script>
 export default {
   data() {
-    return {
-
-    }
+    return {};
   },
   methods: {
     open() {
-        this.$message('这是一条消息提示');
-      },
-      open2() {
-        this.$message({
-          message: '恭喜你，这是一条成功消息',
-          type: 'success'
-        });
-      },
+      this.$message("这是一条消息提示")
+    },
+    open2() {
+      this.$message({
+        message: "恭喜你，这是一条成功消息",
+        type: "success"
+      });
+    },
 
-      open3() {
-        this.$message({
-          message: '警告哦，这是一条警告消息',
-          type: 'warning'
-        });
-      },
+    open3() {
+      this.$message({
+        message: "警告哦，这是一条警告消息",
+        type: "warning"
+      });
+    },
 
-      open4() {
-        this.$message.error('错了哦，这是一条错误消息');
-      }
+    open4() {
+      this.$message.error("错了哦，这是一条错误消息")
+    }
   }
-}
+};
 </script>
 
 <style scoped></style>
